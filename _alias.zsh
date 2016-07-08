fde3dd1aa0b6e7b127446e4110d84a0a09c29ae4
Maximilian Neger <maximilian.neger@nix-wie-weg.de>
2016-07-08 14:02:57 +0200

    update

--- _alias.zsh
+++ _alias.zsh
@@ -6,11 +6,14 @@ alias branch_oldest_ancestor='/usr/bin/diff -u <(git rev-list --first-parent mas
 alias branch_current='git rev-parse --abbrev-ref HEAD'
 alias branch_files='git diff $(branch_oldest_ancestor)..HEAD --diff-filter=ACMR --name-only --oneline'
 alias branch_rubocop='branch_files | grep  "\.\(rb\|gemspec\)$" | xargs rubocop'
+alias connect_dev_vm="ssh nix-wie-weg.max.s5.nix-wie-weg.de"
 
 alias bitbucket_repo_url='echo https://$(git remote -v | grep -oh "bitbucket.org[:/][^ ]\+.git" | head -1 | sed "s/:/\//" | sed "s/\.git$//")'
 alias bitbucket_review_url='echo $(bitbucket_repo_url)/branches/compare/$(branch_current)..$(branch_oldest_ancestor)'
 
 alias vwc="cd /var/www/current"
 alias clip="nc localhost 8377"
+
+alias mount_nww="sshfs mneger@nix-wie-weg.max.s5.nix-wie-weg.de:/var/www/current ~/nww_seite -o idmap=user -o uid=$(id -u) -o gid=$(id -g)"
 
 alias restart_nww="rake assets:clean && rake tmp:clear && touch tmp/restart.txt"
