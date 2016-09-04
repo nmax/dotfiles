905c9aa0af39210e0603a4b9513e77c3afd0d9cb
Maximilian Neger <maximilian.neger@gmail.com>
2016-09-04 09:53:14 +0200

    emacs

---
+++ .emacs.d/.emacs.d
@@ -0,0 +1,1 @@
+/Users/maximilianneger/.emacs.d
---
+++ .emacs.d/elpa/gruvbox-theme-20160514.658/gruvbox-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gruvbox-theme" "20160514.658" "A retro-groove colour theme for Emacs" 'nil :url "http://github.com/Greduan/emacs-theme-gruvbox")
